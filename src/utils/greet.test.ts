01290cba4304e104abcc7ee89b4fa274357834d4
Richard Ng <richardcrng@gmail.com>
2021-06-12 20:22:14 +0100

    Format utils with Prettier

--- src/utils/greet.test.ts
+++ src/utils/greet.test.ts
@@ -1,7 +1,7 @@
-import { greet } from "./greet"
+import { greet } from "./greet";
 
-test('greet returns a string, greeting the passed name', () => {
+test("greet returns a string, greeting the passed name", () => {
   expect(greet("World")).toBe("Hello, World!");
   expect(greet("Richard")).toBe("Hello, Richard!");
-  expect(greet("Academy Scholars")).toBe("Hello, Academy Scholars!")
-})
+  expect(greet("Academy Scholars")).toBe("Hello, Academy Scholars!");
+});
