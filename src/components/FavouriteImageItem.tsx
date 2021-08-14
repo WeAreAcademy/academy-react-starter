2aa503547115663905e89221d538e34b667c7b9f
Rowan Henseleit <rwnhnslt@gmail.com>
2021-08-14 11:35:41 +0100

    added files and stuff

--- src/components/FavouriteImageItem.tsx
+++ src/components/FavouriteImageItem.tsx
@@ -1,29 +1,69 @@
-import { Url } from "url";
+import "./FavouriteImageItem.css";
 
-interface FavouriteImageProps {
-  imgUrl: string;
-  title: string;
-  locationfirst: string;
-  locationsecond: string;
-  maplinkplaceholder: string;
-  mapUrl: string;
-  description: string;
-}
-
-function FavouriteImage(props: FavouriteImageProps): JSX.Element {
+function FavouriteImage(): JSX.Element {
+  const ArrayOfPlaces = [
+    {
+      imgUrl:
+        "https://i.picsum.photos/id/199/536/354.jpg?hmac=g0IyHUDJS4xn7wz_TpnFDJAtrGcukcqV1Ye7w22fdFk",
+      title: "A caravan, in the rain",
+      locationfirst: "Kippford",
+      locationsecond: "Scotland",
+      mapUrl: "urlexample.com",
+      maplinkplaceholder: "map link",
+      description:
+        "I love the noise of the rain on the roof of a caravan, or the canvas of a tent.",
+    },
+    {
+      imgUrl:
+        "https://i.picsum.photos/id/199/536/354.jpg?hmac=g0IyHUDJS4xn7wz_TpnFDJAtrGcukcqV1Ye7w22fdFk",
+      title: "A caravan, in the rain2",
+      locationfirst: "Kippford",
+      locationsecond: "Scotland",
+      mapUrl: "urlexample.com",
+      maplinkplaceholder: "map link",
+      description:
+        "I love the noise of the rain on the roof of a caravan, or the canvas of a tent.",
+    },
+    {
+      imgUrl:
+        "https://i.picsum.photos/id/199/536/354.jpg?hmac=g0IyHUDJS4xn7wz_TpnFDJAtrGcukcqV1Ye7w22fdFk",
+      title: "A caravan, in the rain2",
+      locationfirst: "Kippford",
+      locationsecond: "Scotland",
+      mapUrl: "urlexample.com",
+      maplinkplaceholder: "map link",
+      description:
+        "I love the noise of the rain on the roof of a caravan, or the canvas of a tent.",
+    },
+    {
+      imgUrl:
+        "https://i.picsum.photos/id/199/536/354.jpg?hmac=g0IyHUDJS4xn7wz_TpnFDJAtrGcukcqV1Ye7w22fdFk",
+      title: "A caravan, in the rain2",
+      locationfirst: "Kippford",
+      locationsecond: "Scotland",
+      mapUrl: "urlexample.com",
+      maplinkplaceholder: "map link",
+      description:
+        "I love the noise of the rain on the roof of a caravan, or the canvas of a tent.",
+    },
+  ];
   return (
     <section>
-      <img src={props.imgUrl}></img>
-      <h3>{props.title}</h3>
-      <div>
-        <b>
-          {props.locationfirst}, {props.locationsecond}
-        </b>{" "}
-        <b>(</b>
-        <a href={props.mapUrl}>{props.maplinkplaceholder}</a>
-        <b>)</b>
-      </div>
-      <article>{props.description}</article>
+      {ArrayOfPlaces.map((place) => (
+        <div key={place.title}>
+          <img src={place.imgUrl}></img>
+          <h3>{place.title}</h3>
+          <div>
+            <b>
+              {place.locationfirst}, {place.locationsecond}
+            </b>{" "}
+            <b>(</b>
+            <a href={place.mapUrl}>{place.maplinkplaceholder}</a>
+            <b>)</b>
+          </div>
+          <article>{place.description}</article>
+        </div>
+      ))}
     </section>
   );
 }
--- src/components/FavouriteImages.tsx
+++ src/components/FavouriteImages.tsx
@@ -3,58 +3,7 @@ import FavouriteImageItem from "./FavouriteImageItem";
 function FavouriteImages(): JSX.Element {
   return (
     <section>
-      <FavouriteImageItem
-        imgUrl={
-          "https://i.picsum.photos/id/199/536/354.jpg?hmac=g0IyHUDJS4xn7wz_TpnFDJAtrGcukcqV1Ye7w22fdFk"
-        }
-        title={"A caravan, in the rain"}
-        locationfirst={"Kippford"}
-        locationsecond={"Scotland"}
-        mapUrl={"urlexample.com"}
-        maplinkplaceholder={"map link"}
-        description={
-          "I love the noise of the rain on the roof of a caravan, or the canvas of a tent."
-        }
-      />
-      <FavouriteImageItem
-        imgUrl={
-          "https://i.picsum.photos/id/1013/536/354.jpg?hmac=g-noHNvrLjQNz62ByvWlOk-g2K4_LNhFKl8THdi9FU8"
-        }
-        title={"A caravan, in the rain"}
-        locationfirst={"Kippford"}
-        locationsecond={"Scotland"}
-        mapUrl={"https://warmm.co.uk"}
-        maplinkplaceholder={"map link"}
-        description={
-          "I love the noise of the rain on the roof of a caravan, or the canvas of a tent."
-        }
-      />
-      <FavouriteImageItem
-        imgUrl={
-          "https://i.picsum.photos/id/314/536/354.jpg?hmac=uQME2VgtwoULPclwqxIs5OqNE84y_4z4uIGmMgynbLU"
-        }
-        title={"A caravan, in the rain"}
-        locationfirst={"Kippford"}
-        locationsecond={"Scotland"}
-        mapUrl={"urlexample.com"}
-        maplinkplaceholder={"map link"}
-        description={
-          "I love the noise of the rain on the roof of a caravan, or the canvas of a tent."
-        }
-      />
-      <FavouriteImageItem
-        imgUrl={
-          "https://i.picsum.photos/id/164/536/354.jpg?hmac=hpetWeo-NPRH4DI1ZzpBXB3ibSntdU3FOW2ICJ771nY"
-        }
-        title={""}
-        locationfirst={"Kippford"}
-        locationsecond={"Scotland"}
-        mapUrl={"urlexample.com"}
-        maplinkplaceholder={"map link"}
-        description={
-          "I love the noise of the rain on the roof of a caravan, or the canvas of a tent."
-        }
-      />
+      <FavouriteImageItem />
     </section>
   );
 }
