8ff7e13939e68cffc89c88990eb49d2a667ecce3
Soso Ogan <soso737@hotmail.co.uk>
2022-03-05 18:13:00 +0000

    page links

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -5,17 +5,18 @@ function MainContent(): JSX.Element {
   return (
     <main>
     <section 
-        style={{color: "#d6cbd3",backgroundColor: "#3e4444", margin:12 , padding: 15 }}>
+        style={{color: "#d6cbd3",backgroundColor: "#3e4444", margin:12 , padding: 15, textAlign: "center" }}>
         <img src="/images/firstImage.jpg" width={450} height={450} alt="Big orange canyon in Arizona" />
         <PlaceEntry
             title={"Antelope Canyon"}
             city={"Arizona"}
-            country={"USA"}
+            country={"USA"} 
             text={"I feel as though I am in a different world in this canyon"}
         />
+        <a href="https://goo.gl/maps/4QXMir55kfaGnhbr6">(map link)</a>
     </section>
 
-    <section  style={{color: "#d6cbd3",backgroundColor: "#3e4444", margin:12, padding: 15,}}>
+    <section  style={{color: "#d6cbd3",backgroundColor: "#3e4444", margin:0, padding: 2,textAlign: "center"}}>
         <img src="/images/secondImage.jpg" width={450} height={450} alt="Beach front" />
         <PlaceEntry
             title={"White sands and blue waters"}
@@ -23,9 +24,10 @@ function MainContent(): JSX.Element {
             country={"Maldives"}
             text={"I love spending the day at the beach swimming in the sea"}
         />
+        <a href="https://goo.gl/maps/xUBAwiiiAgviQjTt5">(map link)</a>
     </section>
 
-    <section  style={{color: "#d6cbd3",backgroundColor: "#3e4444", margin: 12, padding: 15,}}>
+    <section  style={{color: "#d6cbd3",backgroundColor: "#3e4444", margin: 12, padding: 15,textAlign: "center"}}>
         <img src="/images/thirdImage.jpg" width={450} height={450} alt="Big orange canyon in Arizona" />
         <PlaceEntry
             title={"Green country"}
@@ -33,6 +35,7 @@ function MainContent(): JSX.Element {
             country={"Northern Ireland"}
             text={"I love beautiful pastures of green spaces"}
         />
+        <a href="https://goo.gl/maps/3pnDe54rsZFrGmtG8">(map link)</a>
     </section>
     </main>
   );
--- src/components/PlaceEntry.tsx
+++ src/components/PlaceEntry.tsx
@@ -11,10 +11,8 @@ interface PlaceEntryProps {
 function PlaceEntry(props: PlaceEntryProps): JSX.Element {
   return (
     <section style={{color: "orange"}}>
-    <h1 style={{fontSize:50}}>{props.title}</h1>
-      <p style={{fontSize:30}}>
-        <strong>{props.city}, {props.country}</strong>
-      </p>
+      <p style={{fontSize:40}}>{props.title}</p>
+      <p style={{fontSize:30}}><strong>{props.city}, {props.country}</strong></p>
       <p style={{fontSize:20}}>{props.text}</p>
     </section>
   );
