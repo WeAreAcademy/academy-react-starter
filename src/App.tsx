a7f346dd7f088a75740e66021b1ca1c8a37d6aa9
Zac Gladman <zacharygladman@yahoo.co.uk>
2022-11-04 12:35:34 +0000

    remove util files and add empty files for AppHeader and MainContent

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import { greet } from "./utils/greet";
+
+import AppHeader from "./components/AppHeader";
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+  
+  return (
+    <>
+      <AppHeader />;
+    </>
+  )
 }
 
 export default App;
--- src/utils/greet.test.ts
+++
@@ -1,7 +0,0 @@
-import { greet } from "./greet";
-
-test("greet returns a string, greeting the passed name", () => {
-  expect(greet("World")).toBe("Hello, World!");
-  expect(greet("Richard")).toBe("Hello, Richard!");
-  expect(greet("Academy Scholars")).toBe("Hello, Academy Scholars!");
-});
