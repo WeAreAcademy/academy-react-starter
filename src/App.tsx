d20769424c84ca6552de7f0ac17ff5bfeb68998a
Maria <mtens17@gmail.com>
2023-01-07 13:50:29 +0000

    Format and lint

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ interface ITask {
   id: number;
   time: Date;
 }
-export type View = "TodoTasks"|"CompletedTasks"
+export type View = "TodoTasks" | "CompletedTasks";
 
 function App(): JSX.Element {
   const [input, setInput] = useState<string>("");
@@ -49,126 +49,147 @@ function App(): JSX.Element {
     await fetchTasks();
     setInput("");
   };
-  if (view === "TodoTasks"){
-  return (
-    <>
-    <NavBar setView={setView}/>
-      <div className = "input-div">
-        <textarea className="inputBox"
-          placeholder="Write your task here"
-          value={input}
-          onChange={(event) => {
-            handleToDoInput(event.target.value);
-          }}
-        ></textarea>
-<span>
-      <button className = "add-button"
-          onClick={handleEnter}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              handleEnter();
-              console.log("key working");
-            }
-          }}
-          ><span className = "plus-sign">+</span>
-        </button></span>
-      </div>
-      <div className = "task-ctn">
-      {/* saved todos */}
-      {tasks &&
-        tasks.map((task) => (
-          <div className = "task" key={task.id}>
-              <div className = "task-txt" id={String(task.id)} contentEditable={contentEditable}>
-                {task.task} 
-              </div>
-              <div className = "btn-ctn">
-              <small className = "time"> {task.time}</small>
-              {/* button to delete */}
-              <button className = "del-btn"
-                onClick={async () => {
-                  await axios.delete(
-                    `https://mariatens-todo-sql-backend.onrender.com/tasks/${task.id}`
-                  );
-                  fetchTasks();
-                }}
-              >
-                🗑️
-              </button>
-              {/* button to change  */}
-              <button
-                onClick={async () => {
-                  setContentEditable(!contentEditable);
-                  const container = document.getElementById(String(task.id));
-                  if (container?.textContent) {
-                    await axios.patch(
-                      `https://mariatens-todo-sql-backend.onrender.com/tasks/${task.id}`,
-                      { task: container?.textContent }
-                    );
-                    setEditedText(container.textContent);
-                    if (editedText) {
-                      //if text edited, display it
-                      task.task = editedText;
-                    }
-                  }
-                }}
-              >
-                ✍️
-              </button>
-              {/* button to mark as complete*/}
-              <button
-                onClick={async () => {
-                  await axios.post(
-                    "https://mariatens-todo-sql-backend.onrender.com/completed-tasks",
-                    {
-                      task: task.task,
-                      time: new Date().toISOString().substring(1, 10),
-                    }
-                  );
-                  fetchTasks();
-                  await axios.delete(
-                    `https://mariatens-todo-sql-backend.onrender.com/tasks/${task.id}`
-                  );
-                  fetchCompletedTasks();
-                }}
-              >
-                ✔️
-              </button>
-              </div>
-          </div>
-        ))}</div></>)}
-        else {
-          return(
-          <>
-            <NavBar setView={setView}/>
-      <div className = "input-div">
-        <textarea className="inputBox"
-          placeholder="Write your task here"
-          value={input}
-          onChange={(event) => {
-            handleToDoInput(event.target.value);
-          }}
-        ></textarea>
-<span>
-      <button className = "add-button"
-          onClick={handleEnter}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              handleEnter();
-              console.log("key working");
-            }
-          }}
-          ><span className = "plus-sign">+</span>
-        </button></span>
-      </div>
-      <h2 className="title">Completed tasks</h2>
-      <div className = "task-ctn">
-      {completedTasks.map((compTask) => (
-        <div className="task"  key={compTask.id}>
-        <div>
-            <s>{compTask.task}</s>
+  if (view === "TodoTasks") {
+    return (
+      <>
+        <NavBar setView={setView} />
+        <div className="input-div">
+          <textarea
+            className="inputBox"
+            placeholder="Write your task here"
+            value={input}
+            onChange={(event) => {
+              handleToDoInput(event.target.value);
+            }}
+          ></textarea>
+          <span>
+            <button
+              className="add-button"
+              onClick={handleEnter}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  handleEnter();
+                  console.log("key working");
+                }
+              }}
+            >
+              <span className="plus-sign">+</span>
+            </button>
+          </span>
         </div>
+        <div className="task-ctn">
+          {/* saved todos */}
+          {tasks &&
+            tasks.map((task) => (
+              <div className="task" key={task.id}>
+                <div
+                  className="task-txt"
+                  id={String(task.id)}
+                  contentEditable={contentEditable}
+                >
+                  {task.task}
+                </div>
+                <div className="btn-ctn">
+                  <small className="time"> {task.time}</small>
+                  {/* button to delete */}
+                  <button
+                    className="del-btn"
+                    onClick={async () => {
+                      await axios.delete(
+                        `https://mariatens-todo-sql-backend.onrender.com/tasks/${task.id}`
+                      );
+                      fetchTasks();
+                    }}
+                  >
+                    🗑️
+                  </button>
+                  {/* button to change  */}
+                  <button
+                    onClick={async () => {
+                      setContentEditable(!contentEditable);
+                      const container = document.getElementById(
+                        String(task.id)
+                      );
+                      if (container?.textContent) {
+                        await axios.patch(
+                          `https://mariatens-todo-sql-backend.onrender.com/tasks/${task.id}`,
+                          { task: container?.textContent }
+                        );
+                        setEditedText(container.textContent);
+                        if (editedText) {
+                          //if text edited, display it
+                          task.task = editedText;
+                        }
+                      }
+                    }}
+                  >
+                    ✍️
+                  </button>
+                  {/* button to mark as complete*/}
+                  <button
+                    onClick={async () => {
+                      await axios.post(
+                        "https://mariatens-todo-sql-backend.onrender.com/completed-tasks",
+                        {
+                          task: task.task,
+                          time: new Date().toISOString().substring(1, 10),
+                        }
+                      );
+                      fetchTasks();
+                      await axios.delete(
+                        `https://mariatens-todo-sql-backend.onrender.com/tasks/${task.id}`
+                      );
+                      fetchCompletedTasks();
+                    }}
+                  >
+                    ✔️
+                  </button>
+                </div>
+              </div>
+            ))}
         </div>
-      ))}</div>
       </>
-  )}}
+    );
+  } else {
+    return (
+      <>
+        <NavBar setView={setView} />
+        <div className="input-div">
+          <textarea
+            className="inputBox"
+            placeholder="Write your task here"
+            value={input}
+            onChange={(event) => {
+              handleToDoInput(event.target.value);
+            }}
+          ></textarea>
+          <span>
+            <button
+              className="add-button"
+              onClick={handleEnter}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  handleEnter();
+                  console.log("key working");
+                }
+              }}
+            >
+              <span className="plus-sign">+</span>
+            </button>
+          </span>
+        </div>
+        <h2 className="title">Completed tasks</h2>
+        <div className="task-ctn">
+          {completedTasks.map((compTask) => (
+            <div className="task" key={compTask.id}>
+              <div>
+                <s>{compTask.task}</s>
+              </div>
+            </div>
+          ))}
+        </div>
+      </>
+    );
+  }
+}
 export default App;
--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,18 +1,22 @@
 import { View } from "./App";
 
-interface NavBarProps{
-    setView: React.Dispatch<React.SetStateAction<View>>;
+interface NavBarProps {
+  setView: React.Dispatch<React.SetStateAction<View>>;
 }
 
-
-export function NavBar({setView}: NavBarProps): JSX.Element{
-    return (
-        <div className = "navbar">
-        <button className = "navbar-btns" onClick ={()=> setView("CompletedTasks")}>Completed Tasks | </button>
-        <button className = "navbar-btns" onClick ={()=> setView("TodoTasks")}>To Do Tasks</button>
-        <h1 className="title" onClick ={()=> setView("TodoTasks")}> TO DO APP </h1>
-        
-        </div>
-    )
-
+export function NavBar({ setView }: NavBarProps): JSX.Element {
+  return (
+    <div className="navbar">
+      <button className="navbar-btns" onClick={() => setView("CompletedTasks")}>
+        Completed Tasks |{" "}
+      </button>
+      <button className="navbar-btns" onClick={() => setView("TodoTasks")}>
+        To Do Tasks
+      </button>
+      <h1 className="title" onClick={() => setView("TodoTasks")}>
+        {" "}
+        TO DO APP{" "}
+      </h1>
+    </div>
+  );
 }
