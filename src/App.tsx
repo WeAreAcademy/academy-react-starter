4adeceabcdc60b43ccf67267aa84d47c8063cf0c
Maria <mtens17@gmail.com>
2023-01-07 12:40:38 +0000

    Format and lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,112 +1,150 @@
-import './App.css'
+import "./App.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
 interface ITask {
-  task: string
-  id: number
-  time: Date
+  task: string;
+  id: number;
+  time: Date;
 }
 
 function App(): JSX.Element {
   const [input, setInput] = useState<string>("");
-  const [tasks, setTasks] = useState<ITask[]>([])
-  const [editedText, setEditedText] = useState<string>()
-  const [completedTasks, setCompletedTasks] = useState<ITask[]>([])
-  const [contentEditable, setContentEditable] = useState(false)
+  const [tasks, setTasks] = useState<ITask[]>([]);
+  const [editedText, setEditedText] = useState<string>();
+  const [completedTasks, setCompletedTasks] = useState<ITask[]>([]);
+  const [contentEditable, setContentEditable] = useState(false);
   const fetchTasks = async () => {
     const response = await fetch(
       "https://mariatens-todo-sql-backend.onrender.com/tasks"
     );
     const jsonBody = await response.json();
     setTasks(jsonBody);
-  }
+  };
   useEffect(() => {
-    fetchTasks()
-  }, [completedTasks] 
-  )
+    fetchTasks();
+  }, [completedTasks]);
   const fetchCompletedTasks = async () => {
     const response = await fetch(
       "https://mariatens-todo-sql-backend.onrender.com/completed-tasks"
     );
     const jsonBody = await response.json();
     setCompletedTasks(jsonBody);
-  }
+  };
   useEffect(() => {
-    fetchCompletedTasks()
-  }, [] 
-  )
+    fetchCompletedTasks();
+  }, []);
 
-// const handleCompleted =  (task: ITask) => {
-//   setCompletedTasks([...completedTasks, task])}
-const handleToDoInput = (toDoInput: string) => {
+  // const handleCompleted =  (task: ITask) => {
+  //   setCompletedTasks([...completedTasks, task])}
+  const handleToDoInput = (toDoInput: string) => {
     setInput(toDoInput);
   };
   const handleEnter = async () => {
-      await axios.post("https://mariatens-todo-sql-backend.onrender.com/tasks",
-       { task: input, 
-        time: new Date().toISOString().substring(1,10) })
-       fetchTasks()
-       setInput("")
-      }
+    await axios.post("https://mariatens-todo-sql-backend.onrender.com/tasks", {
+      task: input,
+      time: new Date().toISOString().substring(1, 10),
+    });
+    await fetchTasks();
+    setInput("");
+  };
 
-  return (<>
-    <h1 className="title"> TO DO APP </h1>
-    <div className = "inputBox"> 
-    <input placeholder="Write your task here"
-      value={input}
-      onChange={(event) => { handleToDoInput(event.target.value) }}></input>
-    
-    <button 
-    onClick={handleEnter} 
-    onKeyDown={(e)=> {if ((e.key)==="Enter"){
-      handleEnter()
-      console.log("key working")
-    }}}>
-      +</button>
-    </div>
-    {/* saved todos */}
-    {tasks && tasks.map(task => 
-    <ul key={task.id}>  
-      <li><div id = {String(task.id)} contentEditable={contentEditable}> 
-      {task.task} <small> {task.time}</small></div> 
-         {/* button to delete */}
-        <button onClick={async () => { 
-        await axios.delete(`https://mariatens-todo-sql-backend.onrender.com/tasks/${task.id}`)
-        fetchTasks()
-        }}>🗑️</button>
-        {/* button to change  */}
-        <button onClick={async () => { 
-        setContentEditable(!contentEditable)
-        const container = document.getElementById(String(task.id));
-        if (container?.textContent){
-          await axios.patch(`https://mariatens-todo-sql-backend.onrender.com/tasks/${task.id}`, {task: container?.textContent})
-        setEditedText(container.textContent)
-          if (editedText){ //if text edited, display it 
-            task.task = editedText
-          }
-        }
-        }}>✍️</button>
-      {/* button to mark as complete*/}
-        <button onClick = {async () => {
-          await axios.post("https://mariatens-todo-sql-backend.onrender.com/completed-tasks",
-           {task:task.task,
-            time: new Date().toISOString().substring(1,10)})
-            fetchTasks()
-          await axios.delete(`https://mariatens-todo-sql-backend.onrender.com/tasks/${task.id}`)
-          fetchCompletedTasks()
-      }
-      }>✔️</button>
-      </li>
-    </ul>)}
-    <hr className = "completed"></hr>
-    <h2 className = "title">Completed tasks</h2>
-    {completedTasks.map(compTask => 
-      <ul key = {compTask.id}>
-        <li><s>{compTask.task}</s></li>
-      </ul>)}
+  return (
+    <>
+      <h1 className="title"> TO DO APP </h1>
+      <div className="inputBox">
+        <input
+          placeholder="Write your task here"
+          value={input}
+          onChange={(event) => {
+            handleToDoInput(event.target.value);
+          }}
+        ></input>
 
-  </>)
+        <button
+          onClick={handleEnter}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              handleEnter();
+              console.log("key working");
+            }
+          }}
+        >
+          +
+        </button>
+      </div>
+      {/* saved todos */}
+      {tasks &&
+        tasks.map((task) => (
+          <ul key={task.id}>
+            <li>
+              <div id={String(task.id)} contentEditable={contentEditable}>
+                {task.task} <small> {task.time}</small>
+              </div>
+              {/* button to delete */}
+              <button
+                onClick={async () => {
+                  await axios.delete(
+                    `https://mariatens-todo-sql-backend.onrender.com/tasks/${task.id}`
+                  );
+                  fetchTasks();
+                }}
+              >
+                🗑️
+              </button>
+              {/* button to change  */}
+              <button
+                onClick={async () => {
+                  setContentEditable(!contentEditable);
+                  const container = document.getElementById(String(task.id));
+                  if (container?.textContent) {
+                    await axios.patch(
+                      `https://mariatens-todo-sql-backend.onrender.com/tasks/${task.id}`,
+                      { task: container?.textContent }
+                    );
+                    setEditedText(container.textContent);
+                    if (editedText) {
+                      //if text edited, display it
+                      task.task = editedText;
+                    }
+                  }
+                }}
+              >
+                ✍️
+              </button>
+              {/* button to mark as complete*/}
+              <button
+                onClick={async () => {
+                  await axios.post(
+                    "https://mariatens-todo-sql-backend.onrender.com/completed-tasks",
+                    {
+                      task: task.task,
+                      time: new Date().toISOString().substring(1, 10),
+                    }
+                  );
+                  fetchTasks();
+                  await axios.delete(
+                    `https://mariatens-todo-sql-backend.onrender.com/tasks/${task.id}`
+                  );
+                  fetchCompletedTasks();
+                }}
+              >
+                ✔️
+              </button>
+            </li>
+          </ul>
+        ))}
+      <hr className="completed"></hr>
+      <h2 className="title">Completed tasks</h2>
+      {completedTasks.map((compTask) => (
+        <ul key={compTask.id}>
+          <li>
+            <s>{compTask.task}</s>
+          </li>
+        </ul>
+      ))}
+    </>
+  );
 }
 
 export default App;
