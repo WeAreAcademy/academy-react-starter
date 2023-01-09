9b31f685acf1c5b99c90bd13f3b0709c3cd180c7
nbogie <neillbogie@googlemail.com>
2023-01-09 23:04:10 +0000

    re-added sample react-testing-library test and setup

---
+++ src/App.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders greeting', () => {
+  render(<App />);
+  const soughtElement = screen.getByText(/Hello, World!/i);
+  expect(soughtElement).toBeInTheDocument();
+});
